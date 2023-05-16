f6921cd062fb178aed59d3707a01b0ac09db6f88
hanbyeol kwon <janviertrois@naver.com>
2023-05-16 13:19:10 -0400

    Add emailjs, Add contact form

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './components/pages/Home';
+import Contact from './components/pages/Contact';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           <Navbar />
           <Routes>
             <Route path='/' exact element={<Home />} />
+            <Route path='/contact' exact element={<Contact />} />
           </Routes>
         </Router>
       </div>
