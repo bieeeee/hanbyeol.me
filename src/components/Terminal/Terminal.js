411cefebc901b86020c9d68ad9ae5bf64b0f0e3e
hanbyeol kwon <janviertrois@naver.com>
2023-10-18 18:27:08 +0900

    Install react-type-animation, Add type-animation to teminal, delete typewriter cdn

--- src/components/Terminal/Terminal.js
+++ src/components/Terminal/Terminal.js
@@ -1,6 +1,6 @@
-import React, { useState, lazy, Suspense } from 'react'
+import React, { useState } from 'react'
 import './Terminal.css';
-const Typewriter = lazy(() => import('typewriter-effect'));
+import { TypeAnimation } from 'react-type-animation';
 
 function Terminal() {
   const [isClosed, setIsClosed] = useState(false);
@@ -21,17 +21,16 @@ function Terminal() {
             </button>
           </div>
           <div className="terminal-content">
-            <Suspense fallback={<div>Loading...</div>}>
-              <Typewriter
-                options={{
-                  strings: ["Hello, bonjour!<br>Welcome to my 90s-inspired screen, where web development meets nostalgia.<br>I go by Hanbyeol (but you can call me Bie).<br>Besides coding, I dabble in DIY, baking, camping, and being the best dog mom ever!<br>If you're up for a fun project, feel free to reach out through this website or connect with me on LinkedIn.<br>Have a nice day ou bonne soirée! :)"],
-                  autoStart: true,
-                  delay: 30,
-                  loop: false,
-                  deleteSpeed: Infinity
-                }}
+              <TypeAnimation
+                sequence={[
+                  "Hello, bonjour!\nWelcome to my 90s-inspired screen, where web development meets nostalgia.\nI go by Hanbyeol (but you can call me Bie).\nBesides coding, I dabble in DIY, baking, camping, and being the best dog mom ever!\nIf you're up for a fun project, feel free to reach out through this website or connect with me on LinkedIn.\nHave a nice day ou bonne soirée! :)", // Types 'One'
+                  1000,
+                ]}
+                wrapper="span"
+                cursor={true}
+                repeat={Infinity}
+                style={{ whiteSpace: 'pre-line', display: 'inline-block' }}
               />
-            </Suspense>
           </div>
         </div>
       )}
