ec8b395af3aa0558c66997977d9e108c247e1f7d
hanbyeol kwon <janviertrois@naver.com>
2023-10-29 11:45:14 +0900

    Implement startTyping when window.onload, Change sequence, set width, height of close button

--- src/components/Terminal/Terminal.js
+++ src/components/Terminal/Terminal.js
@@ -1,13 +1,21 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './Terminal.css';
 import { TypeAnimation } from 'react-type-animation';
 
 function Terminal() {
   const [isClosed, setIsClosed] = useState(false);
+  const [startTyping, setStartTyping] = useState(false);
 
   const handleClose = () => {
     setIsClosed(true);
   }
+
+  useEffect(() => {
+    const handleLoad = () => setStartTyping(true);
+    window.onload = handleLoad;
+    setStartTyping(true);
+  }, []);
+
 
   return (
     <>
@@ -17,20 +25,20 @@ function Terminal() {
             <img src='assets/icons/prompt.png?as=webp' alt='folder' style={{width: '20px', height: '20px'}} />
             <p>About Me</p>
             <button className='btn-close' onClick={handleClose}>
-              <img src="assets/icons/close.png?as=webp" alt="close" />
+              <img src="assets/icons/close.png?as=webp" alt="close" style={{width: '14.5px', height: '16px'}} />
             </button>
           </div>
           <div className="terminal-content">
-              <TypeAnimation
+              {startTyping && <TypeAnimation
                 sequence={[
-                  "Hello, bonjour!\nWelcome to my 90s-inspired screen, where web development meets nostalgia.\nI go by Hanbyeol (but you can call me Bie).\nBesides coding, I dabble in DIY, baking, camping, and being the best dog mom ever!\nIf you're up for a fun project, feel free to reach out through this website or connect with me on LinkedIn.\nHave a nice day ou bonne soirée! :)", // Types 'One'
-                  1000,
+                  "Hello, bonjour!\nMy name is Hanbyeol(or Bie).\nWelcome to my 90s-inspired screen, where web development meets nostalgia.\nBesides coding, I dabble in DIY, baking, camping, and being the best dog mom ever!\nIf you're up for a fun project, feel free to reach out through this website or connect with me on LinkedIn.\nHave a nice day ou bonne soirée! :)", // Types 'One'
+                  1500,
                 ]}
                 wrapper="span"
                 cursor={true}
                 repeat={Infinity}
                 style={{ whiteSpace: 'pre-line', display: 'inline-block' }}
-              />
+              />}
           </div>
         </div>
       )}
