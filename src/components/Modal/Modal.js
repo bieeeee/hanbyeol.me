b569fedfc1c5239a307a7f3b73e37dc54a9e9a7e
hanbyeol kwon <janviertrois@naver.com>
2023-10-29 11:59:32 +0900

    Remove ?as=webp

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -32,7 +32,7 @@ function Modal() {
     <>
       <div className='folders'>
         <div onClick={toggleSkillModal} className='btn-modal'>
-          <img rel="preload" src='assets/icons/folder.png?as=webp' alt='folder' style={{width: '48px', height: '48px'}} />
+          <img rel="preload" src='assets/icons/folder.png' alt='folder' style={{width: '48px', height: '48px'}} />
           <h3>Skills</h3>
         </div>
 
@@ -41,7 +41,7 @@ function Modal() {
               <div onClick={toggleSkillModal} className='overlay'>
                 <div className='modal-container'>
                   <div className="modal-bar">
-                    <img rel="preload" src="assets/icons/folder-open.png?as=webp" alt="folder" />
+                    <img src="assets/icons/folder-open.png?as=webp" alt="folder" />
                     <p>Skills</p>
                     <button onClick={toggleSkillModal} className='close-modal'>
                       <img src="assets/icons/close.png?as=webp" alt="close" />
@@ -80,21 +80,21 @@ function Modal() {
         }
 
         {/* <a href="assets/resume.pdf" target='_blank' rel="preload noreferrer noopener" className='btn-modal' style={{textDecoration: 'none', color: 'black'}}>
-          <img rel="preload" src="assets/icons/notepad.png?as=webp" alt="resume" style={{width: '48px'}} />
+          <img rel="preload" src="assets/icons/notepad.png" alt="resume" style={{width: '48px'}} />
           <p>Resume</p>
         </a> */}
 
         <a href="https://www.linkedin.com/in/hanbyeol-kwon/" target='_blank' rel="noreferrer noopener" className='btn-modal' style={{textDecoration: 'none', color: 'black'}}>
-          <img rel="preload" src="assets/icons/linkedin.png?as=webp" alt="linkedin" style={{width: '48px', height: '48px'}} />
+          <img rel="preload" src="assets/icons/linkedin.png" alt="linkedin" style={{width: '48px', height: '48px'}} />
           <p>LinkedIn</p>
         </a>
 
         <a href="https://github.com/bieeeee" target='_blank' rel="noreferrer noopener" className='btn-modal btn-gh' style={{textDecoration: 'none', color: 'black'}}>
-          <img rel="preload" src="assets/icons/github.png?as=webp" alt="github" style={{width: '48px', height: '48px'}} />
+          <img rel="preload" src="assets/icons/github.png" alt="github" style={{width: '48px', height: '48px'}} />
           <p>Github</p>
         </a>
         <a href="/contact" className='btn-modal btn-gh' style={{textDecoration: 'none', color: 'black'}}>
-          <img rel="preload" src="assets/icons/email.png?as=webp" alt="contact" style={{width: '48px', height: '48px'}} />
+          <img rel="preload" src="assets/icons/email.png" alt="contact" style={{width: '48px', height: '48px'}} />
           <p>Contact</p>
         </a>
       </div>
