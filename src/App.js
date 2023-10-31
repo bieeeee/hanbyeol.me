d38d6fee286e1e3ebf51d7e78da7354ab76eedbe
hanbyeol kwon <janviertrois@naver.com>
2023-10-31 14:25:30 +0900

    Remove lazy load, background-image div

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
 import './App.css';
-import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar/Navbar';
-const Home = lazy(() => import('./components/pages/Home'));
-const Contact = lazy(() => import('./components/pages/Contact'));
+import Home from './components/pages/Home';
 
 function App() {
   return (
-    <>
-      <div className='background-image'>
-        <Router>
-          <Navbar />
-          <Suspense fallback={<div className='fallback'>Loading...</div>}>
-            <Routes>
-              <Route path='/' exact element={<Home />} />
-              <Route path='/contact' exact element={<Contact />} />
-            </Routes>
-          </Suspense>
-        </Router>
-      </div>
-    </>
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path='/' exact element={<Home />} />
+      </Routes>
+    </Router>
   );
 }
 
