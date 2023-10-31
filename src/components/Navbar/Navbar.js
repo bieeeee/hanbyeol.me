6798ec95dd67e3ffe98629b4f3120f1b6ac8ae76
hanbyeol kwon <janviertrois@naver.com>
2023-10-31 17:51:49 +0900

    Removed startAnimation to enable typing animation

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,17 +1,10 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import Dropdown from '../Dropdown/Dropdown';
 import './Navbar.css';
 import Time from '../Time/Time.js';
 
 function Navbar() {
-  const [dropdown, setDropdown] = useState(false);
-  const [startAnimation, setStartAnimation] = useState(false);
-  useEffect(() => {
-    const handleLoad = () => {
-      setStartAnimation(true);
-    };
-    window.onload = handleLoad;
-  }, []);
+  const[dropdown, setDropdown] = useState(false);
 
   const onMouseEnter = () => {
     setDropdown(true);
@@ -28,16 +21,14 @@ function Navbar() {
     <>
       <nav className='navbar'>
         <div className='navbar-container'>
-          <div className='nav-item' onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave} onClick={onClick}>
-            <div className={dropdown ? 'nav-hover' : 'nav-links'}>
-              <img src='assets/icons/logo-icon.png?as=webp' alt='Logo' className='navbar-logo' style={{ width: '19px', height: '19px' }} />
-              Start
+            <div className='nav-item' onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave} onClick={onClick}>
+              <div className={dropdown ? 'nav-hover' : 'nav-links'}>
+                <img src='assets/icons/logo-icon.png?as=webp' alt='Logo' className='navbar-logo' style={{width: '19px', height: '19px'}} />
+                Start
+              </div>
+              {dropdown && <Dropdown />}
             </div>
-            {dropdown && <Dropdown />}
-          </div>
-          <div className='nav-item'>
-            {startAnimation && <Time />}
-          </div>
+            <div className='nav-item'><Time /></div>
         </div>
       </nav>
     </>
