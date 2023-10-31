f52cea60b0a52148b781c6558607bb7f665bd1cd
hanbyeol kwon <janviertrois@naver.com>
2023-10-31 17:11:15 +0900

    Implemented start animation to prevent Time to be loaded before the initial page's loaded

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,19 +1,27 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Dropdown from '../Dropdown/Dropdown';
 import './Navbar.css';
 import Time from '../Time/Time.js';
 
 function Navbar() {
-  const[dropdown, setDropdown] = useState(false);
+  const [dropdown, setDropdown] = useState(false);
+  const [startAnimation, setStartAnimation] = useState(false);
+  useEffect(() => {
+    const handleLoad = () => {
+      setStartAnimation(true);
+    };
+    window.onload = handleLoad;
+    return () => {
+      window.onload = null;
+    };
+  }, []);
 
   const onMouseEnter = () => {
     setDropdown(true);
   };
-
   const onMouseLeave = () => {
     setDropdown(false);
   };
-
   const onClick = () => {
     setDropdown(!dropdown);
   };
@@ -23,14 +31,16 @@ function Navbar() {
     <>
       <nav className='navbar'>
         <div className='navbar-container'>
-            <div className='nav-item' onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave} onClick={onClick}>
-              <div className={dropdown ? 'nav-hover' : 'nav-links'}>
-                <img src='assets/icons/logo-icon.png?as=webp' alt='Logo' className='navbar-logo' style={{width: '19px', height: '19px'}} />
-                Start
-              </div>
-              {dropdown && <Dropdown />}
+          <div className='nav-item' onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave} onClick={onClick}>
+            <div className={dropdown ? 'nav-hover' : 'nav-links'}>
+              <img src='assets/icons/logo-icon.png?as=webp' alt='Logo' className='navbar-logo' style={{ width: '19px', height: '19px' }} />
+              Start
             </div>
-            <div className='nav-item'><Time /></div>
+            {dropdown && <Dropdown />}
+          </div>
+          <div className='nav-item'>
+            {startAnimation && <Time />}
+          </div>
         </div>
       </nav>
     </>
