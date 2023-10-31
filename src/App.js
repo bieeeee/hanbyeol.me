dc73dcf48e00b1e9b5576e9b1fb69da8175000c2
hanbyeol kwon <janviertrois@naver.com>
2023-10-31 15:30:54 +0900

    Refactored routing: Removed unnecessary pages, leaving only the home page in the router configuration

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar/Navbar';
 import Home from './components/pages/Home';
 
 function App() {
   return (
     <Router>
-      <Navbar />
       <Routes>
         <Route path='/' exact element={<Home />} />
       </Routes>
