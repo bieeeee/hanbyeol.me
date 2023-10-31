307344d5c23a8e5db9d3eba3c0ef13597ace8e42
hanbyeol kwon <janviertrois@naver.com>
2023-10-31 16:20:09 +0900

    Refactored App component to directly render the Home page component, simplifying the application structure.

--- src/App.js
+++ src/App.js
@@ -1,14 +1,9 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './components/pages/Home';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path='/' exact element={<Home />} />
-      </Routes>
-    </Router>
+    <Home />
   );
 }
 
