fc8410c9df1f78b223a99d10f6874aa7991da912
hanbyeol kwon <janviertrois@naver.com>
2023-11-02 15:56:52 +0900

    Moved background image to src, Changed bg-img to inline, removed link tag for bg-img, reassigned components container property to each components css files

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { Suspense, lazy } from 'react';
+import bg from './bg.jpg';
 import Navbar from './components/Navbar/Navbar';
 import Terminal from './components/Terminal/Terminal';
 const Modal = lazy(() => import('./components/Modal/Modal'));
@@ -9,7 +10,7 @@ function App() {
   return (
     <div className='home'>
       <Navbar />
-      <div className='sections'>
+      <div className='sections' style={{backgroundImage: `url(${bg})`, backgroundSize: 'cover'}} loading="lazy">
         <Terminal />
         <Suspense fallback={<div>Loading...</div>}>
           <Modal />
