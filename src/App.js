285a9d68daa441197d1177d5ce6b40bb6a44acc8
hanbyeol kwon <janviertrois@naver.com>
2023-11-02 14:55:39 +0900

    Added a script tag to asynchronously load the TypeAnimation library

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import { Suspense, lazy } from 'react';
 import Navbar from './components/Navbar/Navbar';
-const Terminal = lazy(() => import('./components/Terminal/Terminal'));
+import Terminal from './components/Terminal/Terminal';
 const Modal = lazy(() => import('./components/Modal/Modal'));
 const Contact = lazy(() => import('./components/Contact/Contact'));
 
@@ -12,12 +12,10 @@ function App() {
       <div className='sections'>
         <Terminal />
         <Suspense fallback={<div>Loading...</div>}>
-          <Suspense fallback={<div>Loading...</div>}>
-            <Modal />
-          </Suspense>
-          <Suspense fallback={<div>Loading...</div>}>
-            <Contact />
-          </Suspense>
+          <Modal />
+        </Suspense>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Contact />
         </Suspense>
       </div>
     </div>
