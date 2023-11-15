081267f1e429c3d0a8a6a3fed10fad9f26bdc9fe
hanbyeol kwon <janviertrois@naver.com>
2023-11-15 23:38:38 +0900

    Changed hydrate

--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
-import { hydrateRoot } from 'react-dom/client';
+import ReactDOM from 'react-dom/client';
 import App from './App';
 
-hydrateRoot(document.getElementById('root'), <App />);
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+ReactDOM.hydrateRoot(document.getElementById('root'), <App />);
+// root.render(
+//   <App />
+// );
