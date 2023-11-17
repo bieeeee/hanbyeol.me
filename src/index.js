7cfb97c2a35af8947532067bbb852051721962cd
hanbyeol kwon <janviertrois@naver.com>
2023-11-17 15:54:00 +0900

    Initializes i18n

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './App.css';
+import './i18n';
 
 ReactDOM.hydrate(<App />, document.getElementById('root'));
