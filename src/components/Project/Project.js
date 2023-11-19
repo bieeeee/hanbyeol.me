b302b4a19c59de6cf5f793afd8b2b4786a997636
hanbyeol kwon <janviertrois@naver.com>
2023-11-19 23:16:41 +0900

    Set projects variable, refactored repeated codes with map, useState

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,207 +1,70 @@
-import { useState } from 'react'
+import React, { Suspense, lazy, useState } from 'react'
 import "../Modal/Modal.css"
-import VarcharForYou from './VarcharForYou'
-import Hanbyeol from './Hanbyeol'
-import Zapfit from './Zapfit'
-import PlanForYou from './PlanForYou'
-import Onlydevs from './Onlydevs'
-import Booking from './Booking'
+
+const projects = [
+  { name: 'modalOne', title: 'Plan For You', component: lazy(() => import('./PlanForYou')) },
+  { name: 'modalTwo', title: 'Varchar For You', component: lazy(() => import('./VarcharForYou')) },
+  { name: 'modalThree', title: 'Hanbyeol.me', component: lazy(() => import('./Hanbyeol')) },
+  { name: 'modalFour', title: 'Zapfit.ca', component: lazy(() => import('./Zapfit')) },
+  { name: 'modalFive', title: 'OnlyDevs', component: lazy(() => import('./Onlydevs')) },
+  { name: 'modalSix', title: 'Booking.com', component: lazy(() => import('./Booking')) },
+];
 
 function Project() {
-  const [projectModalOne, setProjectModalOne] = useState(false);
-  const [projectModalTwo, setProjectModalTwo] = useState(false);
-  const [projectModalThree, setProjectModalThree] = useState(false);
-  const [projectModalFour, setProjectModalFour] = useState(false);
-  const [projectModalFive, setProjectModalFive] = useState(false);
-  const [projectModalSix, setProjectModalSix] = useState(false);
+  const [projectStates, setProjectStates] = useState(
+    projects.reduce((acc, project) => {
+      acc[project.name] = false;
+      return acc;
+    }, {})
+  );
 
-  const toggleProjectModalOne = (e) => {
+  const toggleProjectModal = (e, name) => {
     e.stopPropagation();
-    setProjectModalOne(!projectModalOne);
-  }
-
-  const toggleProjectModalTwo = (e) => {
-    e.stopPropagation();
-    setProjectModalTwo(!projectModalTwo);
-  }
-
-  const toggleProjectModalThree = (e) => {
-    e.stopPropagation();
-    setProjectModalThree(!projectModalThree);
-  }
-
-  const toggleProjectModalFour = (e) => {
-    e.stopPropagation();
-    setProjectModalFour(!projectModalFour);
-  }
-
-  const toggleProjectModalFive = (e) => {
-    e.stopPropagation();
-    setProjectModalFive(!projectModalFive);
-  }
-
-  const toggleProjectModalSix = (e) => {
-    e.stopPropagation();
-    setProjectModalSix(!projectModalSix);
-  }
+    setProjectStates((prevStates) => ({
+      ...prevStates,
+      [name]: !prevStates[name],
+    }));
+  };
 
   return (
     <>
-      <div
-        onClick={toggleProjectModalOne}
-        className='btn-modal project-modal-icon'>
-        <img src='assets/icons/folder.png' alt='folder' />
-        <p>Plan For You</p>
-      </div>
+      {projects.map((project) => (
+        <div
+          key={project.name}
+          onClick={(e) => toggleProjectModal(e, project.name)}
+          className='btn-modal project-modal-icon'
+        >
+          <img src='assets/icons/folder.png' alt='folder' />
+          <p>{project.title}</p>
+        </div>
+      ))}
 
-      {projectModalOne && (
-        <div className='modal'>
-          <div onClick={toggleProjectModalOne}>
-            <div className='modal-container'>
-              <div className="modal-bar">
-                <img src="assets/icons/folder-open.png" alt="folder" />
-                <p>Plan For You</p>
-                <button onClick={toggleProjectModalOne} className='close-modal'>
-                  <img src="assets/icons/close.png" alt="" style={{ width: '12px', height: '12px' }} />
-                </button>
-              </div>
-              <div className="project-modal-content">
-                <PlanForYou />
+      {projects.map((project) => (
+        <div key={project.name}>
+          {projectStates[project.name] && (
+            <div className='modal'>
+              <div onClick={() => toggleProjectModal(project.name)}>
+                <div className='modal-container'>
+                  <div className="modal-bar">
+                    <div className="modal-bar-left">
+                      <img src="assets/icons/folder-open.png" alt="folder" />
+                      <p>{project.title}</p>
+                    </div>
+                    <button onClick={(e) => toggleProjectModal(e, project.name)} className='close-modal'>
+                      <img src="assets/icons/close.png" alt="" style={{ width: '12px', height: '12px' }} />
+                    </button>
+                  </div>
+                  <div className="project-modal-content">
+                    <Suspense fallback={"loading..."}>
+                      {React.createElement(React.memo(project.component))}
+                    </Suspense>
+                  </div>
+                </div>
               </div>
             </div>
-          </div>
-        </div>)
-      }
-
-      <div
-        onClick={toggleProjectModalTwo}
-        className='btn-modal project-modal-icon'>
-        <img src='assets/icons/folder.png' alt='folder' />
-        <p>Varchar For You</p>
-      </div>
-
-      {projectModalTwo && (
-        <div className='modal'>
-          <div onClick={toggleProjectModalTwo}>
-            <div className='modal-container'>
-              <div className="modal-bar">
-                <img src="assets/icons/folder-open.png" alt="folder" />
-                <p>Varchar For You</p>
-                <button onClick={toggleProjectModalTwo} className='close-modal'>
-                  <img src="assets/icons/close.png" alt="close" style={{ width: '12px', height: '12px' }} />
-                </button>
-              </div>
-              <div className="project-modal-content">
-                <VarcharForYou />
-              </div>
-            </div>
-          </div>
-        </div>)
-      }
-
-      <div
-        onClick={toggleProjectModalThree}
-        className='btn-modal project-modal-icon'>
-        <img src='assets/icons/folder.png' alt='folder' />
-        <p>hanbyeol.me</p>
-      </div>
-
-      {projectModalThree && (
-        <div className='modal'>
-          <div onClick={toggleProjectModalThree}>
-            <div className='modal-container'>
-              <div className="modal-bar">
-                <img src="assets/icons/folder-open.png" alt="folder" />
-                <p>hanbyeol.me</p>
-                <button onClick={toggleProjectModalThree} className='close-modal'>
-                  <img src="assets/icons/close.png" alt="" style={{ width: '12px', height: '12px' }} />
-                </button>
-              </div>
-              <div className="project-modal-content">
-                <Hanbyeol />
-              </div>
-            </div>
-          </div>
-        </div>)
-      }
-
-      <div
-        onClick={toggleProjectModalFour}
-        className='btn-modal project-modal-icon'>
-        <img src='assets/icons/folder.png' alt='folder' />
-        <p>zapfit.ca</p>
-      </div>
-
-      {projectModalFour && (
-        <div className='modal'>
-          <div onClick={toggleProjectModalFour}>
-            <div className='modal-container'>
-              <div className="modal-bar">
-                <img src="assets/icons/folder-open.png" alt="folder" />
-                <p>zapfit.ca</p>
-                <button onClick={toggleProjectModalFour} className='close-modal'>
-                  <img src="assets/icons/close.png" alt=""  style={{ width: '12px', height: '12px' }} />
-                </button>
-              </div>
-              <div className="project-modal-content">
-                <Zapfit />
-              </div>
-            </div>
-          </div>
-        </div>)
-      }
-
-      <div
-        onClick={toggleProjectModalFive}
-        className='btn-modal project-modal-icon'>
-        <img src='assets/icons/folder.png' alt='folder' />
-        <p>OnlyDevs</p>
-      </div>
-
-      {projectModalFive && (
-        <div className='modal'>
-          <div onClick={toggleProjectModalFive}>
-            <div className='modal-container'>
-              <div className="modal-bar">
-                <img src="assets/icons/folder-open.png" alt="folder" />
-                <p>OnlyDevs</p>
-                <button onClick={toggleProjectModalFive} className='close-modal'>
-                  <img src="assets/icons/close.png" alt="" style={{ width: '12px', height: '12px' }} />
-                </button>
-              </div>
-              <div className="project-modal-content">
-                <Onlydevs />
-              </div>
-            </div>
-          </div>
-        </div>)
-      }
-
-      <div
-        onClick={toggleProjectModalSix}
-        className='btn-modal project-modal-icon'>
-        <img src='assets/icons/folder.png' alt='folder' />
-        <p>Booking.com</p>
-      </div>
-
-      {projectModalSix && (
-        <div className='modal'>
-          <div onClick={toggleProjectModalSix}>
-            <div className='modal-container'>
-              <div className="modal-bar">
-                <img src="assets/icons/folder-open.png" alt="folder" />
-                <p>Booking.com</p>
-                <button onClick={toggleProjectModalSix} className='close-modal'>
-                  <img src="assets/icons/close.png" alt="" style={{ width: '12px', height: '12px' }} />
-                </button>
-              </div>
-              <div className="project-modal-content">
-                <Booking />
-              </div>
-            </div>
-          </div>
-        </div>)
-      }
+          )}
+        </div>
+      ))}
     </>
   )
 }
