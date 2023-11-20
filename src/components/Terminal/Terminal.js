98c16cfbecc19280a22b790de68246b1c57ce0b8
hanbyeol kwon <janviertrois@naver.com>
2023-11-20 11:07:12 +0900

    Implemented reload function when language changed

--- src/components/Terminal/Terminal.js
+++ src/components/Terminal/Terminal.js
@@ -1,26 +1,35 @@
-import React, { lazy, Suspense } from 'react'
+import React, { lazy, Suspense, useEffect, useState } from 'react'
 import './Terminal.css';
+import '../Modal/Modal.css';
 const TypeAnimation = lazy(() =>
   window.onload = import("react-type-animation").then(module => {
     return { default: module.TypeAnimation };
   })
 );
-function Terminal({ t }) {
+function Terminal({ t, i18n }) {
+  const [reload, setReload] = useState(0);
+  useEffect(() => {
+    setReload((prev) => prev + 1);
+  }, [i18n.language]);
+
   return (
     <div className='terminal' id='about'>
       <div className='terminal-container'>
-        <div className="terminal-bar">
-          <img src='assets/icons/prompt.png' alt='folder' style={{ width: '20px', height: '20px' }} />
-          About Me
-          <button className='btn-close' aria-label="Close button">
-            <img src="assets/icons/close.png" alt="close-button" style={{ width: '12px', height: '12px' }} />
-          </button>
+        <div className="modal-bar">
+          <div className="modal-bar-left">
+            <img src='assets/icons/prompt.png' alt='folder' />
+            {t('modal.aboutme')}
+          </div>
+          <div className='close-modal' aria-label="Close button">
+            <img src="assets/icons/close.png" alt="close-button" />
+          </div>
         </div>
         <div className="terminal-content">
           <Suspense>
             <TypeAnimation
+              key={reload}
               sequence={[
-                `${t('terminal.desc')}`,
+                t('terminal.desc'),
                 1000,
               ]}
               wrapper="span"
