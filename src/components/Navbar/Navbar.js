75d294f4b47636f348f2ac6ef975574bd4939116
hanbyeol kwon <janviertrois@naver.com>
2023-11-20 11:06:25 +0900

    Added chang language toggle

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,8 +2,12 @@ import React, { useState } from 'react';
 import Dropdown from '../Dropdown/Dropdown';
 import './Navbar.css';
 
-function Navbar({ t }) {
-  const[dropdown, setDropdown] = useState(false);
+function Navbar({ t, i18n }) {
+  const [dropdown, setDropdown] = useState(false);
+  const locales = {
+    ko: { title: 'KO' },
+    en: { title: 'EN' },
+  };
 
   const onMouseEnter = () => {
     setDropdown(true);
@@ -20,13 +24,31 @@ function Navbar({ t }) {
     <>
       <nav className='navbar'>
         <div className='navbar-container'>
-            <div className='nav-item' onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave} onClick={onClick}>
-              <div className={dropdown ? 'nav-hover' : 'nav-links'}>
-                <img rel='preload' src='assets/icons/logo-icon.png' alt='Logo' className='navbar-logo' style={{width: '19px', height: '19px'}} />
-                {t('navbar.start')}
-              </div>
-              {dropdown && <Dropdown t={t} />}
+          <div className='nav-item' onMouseEnter={onMouseEnter} onMouseLeave={onMouseLeave} onClick={onClick}>
+            <div className={dropdown ? 'nav-hover' : 'nav-links'}>
+              <img rel='preload' src='assets/icons/logo-icon.png' alt='Logo' className='navbar-logo' style={{ width: '19px', height: '19px' }} />
+              {t('navbar.start')}
             </div>
+            {dropdown && <Dropdown t={t} />}
+          </div>
+
+          <div className='nav-item'>
+            {Object.keys(locales).map((locale) => (
+              <div
+                className='nav-links'
+                key={locale}
+                style={{ cursor: 'pointer'}}
+              >
+                <div
+                  style={{ fontWeight: i18n.resolvedLanguage === locale ? 'bold' : 'normal' }}
+                  type="submit"
+                  onClick={() => i18n.changeLanguage(locale)}
+                >
+                  {locales[locale].title}
+                </div>
+              </div>
+            ))}
+          </div>
         </div>
       </nav>
     </>
