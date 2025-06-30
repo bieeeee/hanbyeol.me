6229d66cbdaabc19e07a3ea496b9a4c66ab3ea6e
bieeeee <janviertrois@naver.com>
2025-06-30 17:13:44 +0900

    refactor: move dropdown trigger button into Dropdown.tsx

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { useTranslation } from "react-i18next";
+import { DropDown } from "@components";
+import "./Navbar.css";
+
+const locales = [
+  { lang: "ko", title: "KO" },
+  { lang: "en", title: "EN" }
+];
+
+const Navbar: React.FC = () => {
+  const { i18n } = useTranslation();
+
+  return (
+    <nav className="navbar">
+      <DropDown />
+
+      <div className="locales-box">
+        {locales.map(({ lang, title }) => (
+          <button
+            key={lang}
+            className={`btn ${
+              i18n.resolvedLanguage === lang
+                ? "is-resolved clicked"
+                : ""
+            }`}
+            onClick={() => i18n.changeLanguage(lang)}
+          >
+            {title}
+          </button>
+        ))}
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
