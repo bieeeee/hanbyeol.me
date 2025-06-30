cd04f734495e462747f443a47dccaafc7b2e8557
bieeeee <janviertrois@naver.com>
2025-06-30 17:16:03 +0900

    refactor: move dropdown state handling to wrapper

---
+++ src/components/_ui/Dropdown/Dropdown.tsx
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+import { useTranslation } from "react-i18next";
+import "./Dropdown.css";
+
+const MenuItems = [
+  { title: "navbar.about", id: "about" },
+  { title: "navbar.projects", id: "projects" },
+  { title: "navbar.contact", id: "contact" }
+];
+
+const Dropdown: React.FC = () => {
+  const { t } = useTranslation();
+  const [isVisible, setIsVisible] = useState(false);
+
+  const smoothScrollToSection = (sectionId: string) => {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({
+        behavior: "smooth",
+        block: "start"
+      });
+    }
+    setIsVisible(false);
+  };
+
+  return (
+    <div
+      className="dropdown-wrapper"
+      onMouseEnter={() => setIsVisible(true)}
+      onMouseLeave={() => setIsVisible(false)}
+    >
+      <button className="btn">
+        <img
+          src="assets/icons/logo-icon.png"
+          alt="Logo"
+          className="navbar-logo"
+        />
+        {t("navbar.start")}
+      </button>
+
+      <div className="spacing" />
+
+      {isVisible && (
+        <ul className="dropdown-menu">
+          {MenuItems.map((item) => (
+            <li key={item.id}>
+              <span
+                className="dropdown-link"
+                onClick={() =>
+                  smoothScrollToSection(item.id)
+                }
+              >
+                {t(item.title)}
+              </span>
+            </li>
+          ))}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default Dropdown;
