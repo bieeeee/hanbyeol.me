2afaa117656d097c658f2b0bdc373f51519e82b0
bieeeee <janviertrois@naver.com>
2025-07-01 11:15:48 +0900

    refactor: update Terminal to use Modal component

---
+++ src/components/Terminal/Terminal.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { useTranslation } from "react-i18next";
+import { TypeAnimation } from "react-type-animation";
+import { Modal } from "@components";
+import "./Terminal.scss";
+
+const Terminal: React.FC = () => {
+  const { t, i18n } = useTranslation();
+
+  return (
+    <Modal
+      title={t("modal.aboutme")}
+      defaultOpen
+      triggerImg="/src/assets/icons/prompt.png"
+    >
+      <div className="terminal-content">
+        <TypeAnimation
+          key={i18n.language}
+          sequence={[t("terminal.desc"), 1000]}
+          wrapper="span"
+          cursor={true}
+          repeat={Infinity}
+          speed={80}
+          style={{
+            whiteSpace: "pre-line",
+            display: "inline-block"
+          }}
+        />
+      </div>
+    </Modal>
+  );
+};
+
+export default Terminal;
