cdd95103e4436e97550204691ca285b0ccedddf2
bieeeee <janviertrois@naver.com>
2025-07-01 16:58:48 +0900

    refactor: enhance IconBtn component with layout and accessibility props

--- src/components/_ui/IconBtn/IconBtn.tsx
+++ src/components/_ui/IconBtn/IconBtn.tsx
@@ -4,17 +4,30 @@ import "./IconBtn.scss";
 interface IconBtnProps {
   title: string;
   imgSrc: string;
+  isHorizontal?: boolean;
+  role?: string;
+  size?: string;
+  customClass?: string;
   onClick: () => void;
 }
 
 const IconBtn: React.FC<IconBtnProps> = ({
   title,
   imgSrc,
+  isHorizontal,
+  role,
+  size,
+  customClass,
   onClick
 }) => {
   return (
-    <button onClick={onClick} className="icon-btn">
-      <img rel="preload" src={imgSrc} alt={title} />
+    <button
+      onClick={onClick}
+      className={`icon-btn ${isHorizontal ? "horizontal" : ""} ${size} ${customClass}`}
+      title={title}
+      role={role}
+    >
+      <img src={imgSrc} alt={title} />
       <p>{title}</p>
     </button>
   );
