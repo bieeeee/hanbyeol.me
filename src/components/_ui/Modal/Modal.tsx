b99f88432a593db2f3658e154a52b1bb0df681bf
bieeeee <janviertrois@naver.com>
2025-07-01 11:11:17 +0900

    refactor: update Modal to use IconBtn and CloseBtn with optional trigger icon

--- src/components/_ui/Modal/Modal.tsx
+++ src/components/_ui/Modal/Modal.tsx
@@ -1,42 +1,57 @@
-import React, { useState } from 'react'
-import "./Modal.css"
-import FolderBtn from '../FolderBtn/FolderBtn';
+import React, { useState } from "react";
+import { CloseBtn, IconBtn } from "@components";
+import "./Modal.scss";
 
 interface ModalProps {
   title: string;
-  children: React.ReactNode
-} 
-
-const Modal:React.FC<ModalProps> = ({ title, children }) => {
-  const [open, setOpen] = useState(false);
-
-  return (
-    <div className='modals' id='projects'>
-      <div className='folders'>
-        <FolderBtn onClick={() => setOpen(true)} title={title} />
-        {open && (
-          <div className='modal'>
-            <div onClick={() => setOpen(false)} className='overlay'>
-              <div className='modal-container'>
-                <div className="modal-bar">
-                  <div className="modal-bar-left">
-                    <img src="assets/icons/folder-open.png" alt="folder" />
-                    <p>{title}</p>
-                  </div>
-                  <button onClick={() => setOpen(false)} className='close-modal'>
-                    <img src="assets/icons/close.png" alt="" style={{ width: '12px', height: '12px' }} />
-                  </button>
-                </div>
-                <div className="modal-content">
-                  {children}
-                </div>
-              </div>
-            </div>
-          </div>)
-        }
-      </div>
-    </div>
-  );
+  defaultOpen?: boolean;
+  triggerImg?: string;
+  children: React.ReactNode;
 }
 
-export default Modal
+const Modal: React.FC<ModalProps> = ({
+  title,
+  defaultOpen,
+  triggerImg,
+  children
+}) => {
+  const [open, setOpen] = useState(defaultOpen ?? false);
+
+  return (
+    <>
+      <IconBtn
+        imgSrc={
+          triggerImg ?? "/src/assets/icons/folder.png"
+        }
+        onClick={() => setOpen(true)}
+        title={title}
+      />
+      {open && (
+        <div className="modal">
+          <div className="overlay">
+            <div className="modal-container">
+              <div className="modal-bar">
+                <div className="modal-bar-left">
+                  <img
+                    src={
+                      triggerImg ??
+                      "/src/assets/icons/folder-open.png"
+                    }
+                    alt="folder"
+                  />
+                  <p>{title}</p>
+                </div>
+                <CloseBtn onClick={() => setOpen(false)} />
+              </div>
+              <div className="modal-content">
+                {children}
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
+  );
+};
+
+export default Modal;
