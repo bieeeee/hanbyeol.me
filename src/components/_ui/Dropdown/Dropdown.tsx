c1aa33081994e3711662fd9815a5386473250015
bieeeee <janviertrois@naver.com>
2025-07-03 14:24:44 +0900

    feat: change start menu items to external links

--- src/components/_ui/Dropdown/Dropdown.tsx
+++ src/components/_ui/Dropdown/Dropdown.tsx
@@ -3,25 +3,29 @@ import { useTranslation } from "react-i18next";
 import "./Dropdown.scss";
 
 const MenuItems = [
-  { title: "navbar.about", id: "about" },
-  { title: "navbar.projects", id: "projects" },
-  { title: "navbar.contact", id: "contact" }
+  {
+    id: "github",
+    title: "navbar.github",
+    url: "https://github.com/bieeeee",
+    icon: "https://cdn.simpleicons.org/github/383838"
+  },
+  {
+    id: "linkedin",
+    title: "navbar.linkedin",
+    url: "https://www.linkedin.com/in/hanbyeol-kwon",
+    icon: "src/assets/icons/linkedin.png"
+  },
+  {
+    id: "blog",
+    title: "navbar.blog",
+    url: "https://velog.io/@bieeeee",
+    icon: "https://cdn.simpleicons.org/velog"
+  }
 ];
 
 const Dropdown: React.FC = () => {
   const { t } = useTranslation();
   const [isVisible, setIsVisible] = useState(false);
-
-  const smoothScrollToSection = (sectionId: string) => {
-    const section = document.getElementById(sectionId);
-    if (section) {
-      section.scrollIntoView({
-        behavior: "smooth",
-        block: "start"
-      });
-    }
-    setIsVisible(false);
-  };
 
   return (
     <div
@@ -44,14 +48,15 @@ const Dropdown: React.FC = () => {
           <ul className="dropdown-menu">
             {MenuItems.map((item) => (
               <li key={item.id}>
-                <span
+                <a
+                  href={item.url}
                   className="dropdown-link"
-                  onClick={() =>
-                    smoothScrollToSection(item.id)
-                  }
+                  target="_blank"
+                  rel="noopener noreferrer"
                 >
-                  {t(item.title)}
-                </span>
+                  <img src={item.icon} alt={item.id} />
+                  <span>{t(item.title)}</span>
+                </a>
               </li>
             ))}
           </ul>
